400f747c7ff40b2460932fa3315cd94151b4cf27
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2025-02-26 15:54:28 -0300

    chore: setup of project

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+    green: "#257F49",
+    greenSoft: "#D0FFD9",
+    greenDark: "#092917",
+    title: "#1A1F24",
+    gray: "#C4D0DB",
+    danger: "#F94144",
+}
+export default colors
