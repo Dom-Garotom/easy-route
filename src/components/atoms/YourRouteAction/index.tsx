b6896f23efc649042605548cae9ae569280c9e00
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2025-03-01 14:17:29 -0300

    feat: create YourRouteAction component

---
+++ src/components/atoms/YourRouteAction/index.tsx
@@ -0,0 +1,29 @@
+import { IconArrowBarLeft, IconArrowBarRight, IconArrowsMaximize } from '@tabler/icons-react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import colors from '@/styles/colors'
+import { s } from './style'
+
+interface YourRouteActionProps {
+    title: string
+    time: string
+    hasGoing: boolean
+}
+
+export default function YourRouteAction({ hasGoing = true, time, title }: YourRouteActionProps) {
+    return (
+        <TouchableOpacity activeOpacity={0.6} style={s.yourRouteAction_container}>
+            <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+                {hasGoing ?
+                    (<IconArrowBarRight color={colors.green} size={28} />)
+                    :
+                    (<IconArrowBarLeft color={colors.danger} size={28} />)
+                }
+                <View>
+                    <Text style={s.yourRouteAction_title}>{title}</Text>
+                    <Text style={s.yourRouteAction_text}>{time}</Text>
+                </View>
+            </View>
+            <IconArrowsMaximize color={colors.title} size={26} />
+        </TouchableOpacity>
+    )
+}
---
+++ src/components/atoms/YourRouteAction/style.ts
@@ -0,0 +1,27 @@
+import colors from "@/styles/colors";
+import { StyleSheet } from "react-native";
+
+
+export const s = StyleSheet.create({
+    yourRouteAction_container: {
+        width: "100%",
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 12,
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: colors.gray,
+    },
+
+    yourRouteAction_title: {
+        color: colors.title,
+        fontWeight: '600',
+        fontSize: 14,
+    },
+    yourRouteAction_text: {
+        color: colors.text,
+        fontWeight: '500',
+        fontSize: 12,
+    },
+})
